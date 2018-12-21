99f2d833d29f7f68dfcbb670c655f4eab4c54441
009kings <009kings@gmail.com>
2018-12-20 23:05:49 -0800

    just does the hw

--- index.js
+++ index.js
@@ -2,6 +2,26 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var parser = require('body-parser');
+var geocoder = require('simple-geocoder');
+var request = require("request");
+var moment = require('moment');
+
+// Help me
+var wiIcon = {
+  'clear-day': 'wi-day-sunny',
+  'clear-night': 'wi-night-clear',
+  'rain': 'wi-sprinkle',
+  'snow': 'wi-snow',
+  'sleet': 'wi-sleet',
+  'wind': 'wi-cloudy-gusts',
+  'fog': 'wi-fog',
+  'cloudy': 'wi-cloudy',
+  'partly-cloudy-day': 'wi-day-cloudy',
+  'partly-cloudy-night': 'wi-night-alt-cloudy',
+};
+
+// Read in the .env file
+require("dotenv").config();
 
 // Declare your app
 var app = express();
@@ -20,10 +40,30 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-  res.render('result');
+  geocoder.geocode(req.body.locationInput, (success, locations)=>{
+    if (success) {
+      request(`${process.env.DARK_SKY_URL}${locations.y},${locations.x}`, (error, response, body)=>{
+        if (error) {
+          console.log('Bad news bears, there\'s been an error', error);
+        } else {
+          var results = JSON.parse(body);
+
+          // Lets get some DATES
+          var dailyData = results.daily.data;
+          dailyData.splice(5, 3);
+          dailyData.map((dataPack, index) =>{
+            dailyData[index].day = moment.unix(dataPack.time).format('dddd');
+            dailyData[index].wiIcon = wiIcon[dataPack.icon];
+          });
+          
+          res.render('result', {userInput: req.body.locationInput, lat: locations.x, long: locations.y, weather: results, dailyData: dailyData});
+        }
+      })
+    }
+  });
 });
 
 // Listen on PORT 3000
 app.listen(3000, function(){
-  console.log('I\'m listening to the smooth sounds of port 3000 in the morning. ☕');
+  console.log('You\'re listening to the smooth sounds of port 3000. ☕');
 });
